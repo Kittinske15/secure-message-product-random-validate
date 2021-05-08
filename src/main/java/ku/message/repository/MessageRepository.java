af077b6857633374748c904ac099c737197ee250
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-05-09 02:14:59 +0700

    secure product api-auth

--- src/main/java/ku/message/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package ku.message.repository;
-
-import ku.message.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Message,Long> {
-}
